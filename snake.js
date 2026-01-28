c6b688f9295ae2baeb2ef1b14a537eff7ea823d5
iSadChad <tsetenkaikudo@gmail.com>
2026-01-28 11:30:15 +0100

    Update snake.html to include canvas dimensions and initial script; add snake.js for game logic

---
+++ snake.js
@@ -0,0 +1,30 @@
+const blocksize = 20;
+const rows = 30;
+const cols = 30;
+const board = document.getElementById("board");
+const boardContext = board.getContext("2d");
+
+
+let snakeBody = [];
+
+let gameOver = false;
+
+
+window.onload = () => {
+    gameLoop();
+}
+
+function gameLoop() {
+    setInterval(show, 1000/20)
+}
+
+function show() {
+    update();
+    draw();
+}
+
+function update() {
+    if (gameOver) {
+        return;
+    }}
+
